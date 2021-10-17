7bae48643c3042f46f23ad881149ed1af9c2454e
youssef adel <darkmetalxdragon@hotmail.com>
2021-10-17 15:01:26 +0200

    fix some structural errors

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 // import { environment } from "../../../environments/environment";
-import { product, variant } from "../../shared/interface/product";
+import { product } from "../../shared/interface/product";
 import { ProductService } from "../../shared/service/product.service";
 
 @Component({
@@ -9,39 +9,20 @@ import { ProductService } from "../../shared/service/product.service";
   styleUrls: ["./list.component.css"]
 })
 export class ListComponent implements OnInit {
-  // API_URL;
   products: Array<product>;
   displayProducts: Array<product> = [];
   limit = 5;
 
   constructor(private ProductService: ProductService) {
-    // this.API_URL = environment.apiURL;
     this.ProductService.getProducts().subscribe((Products) => {
       this.products = Products;
-      for (var product in Products) {
-        if (
-          this.displayProducts.length < this.limit &&
-          this.isAvailable(Products[product].variants)
-        )
-          this.displayProducts.push(this.products[product]);
-        if (this.displayProducts.length === this.limit) break;
-      }
-      // console.log(this.displayProducts);
+      // this.ProductService.sortProducts(Products);
+      this.displayProducts = this.ProductService.getDisplayProducts(
+        Products,
+        this.limit
+      );
     });
   }
 
   ngOnInit() {}
-
-  /**check if product is available
-   * @param {variant[]} variants
-   * @return {*}  {boolean}
-   * @memberof ListComponent
-   */
-  isAvailable(variants: variant[]): boolean {
-    var res = false;
-    for (var variant in variants) {
-      if (variants[variant].quantity > 0) res = true;
-    }
-    return res;
-  }
 }
--- src/app/shared/service/product.service.ts
+++ src/app/shared/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { product } from "../interface/product";
+import { product, variant } from "../interface/product";
 import { environment } from "../../../environments/environment";
 
 // const httpOptions = {
@@ -23,5 +23,36 @@ export class ProductService {
    */
   getProducts(): Observable<product[]> {
     return this.http.get<product[]>(`${environment.apiURL}/`);
+  }
+
+  /**gets a list of products to display
+   * @param {product[]} Products
+   * @param {number} limit
+   * @return {*} {product[]}
+   * @memberof ListComponent
+   */
+  getDisplayProducts(Products: product[], limit: number): product[] {
+    var displayProducts = [];
+    for (var product in Products) {
+      if (
+        displayProducts.length < limit &&
+        this.isAvailable(Products[product].variants)
+      )
+        displayProducts.push(Products[product]);
+      if (displayProducts.length === limit) break;
+    }
+    return displayProducts;
+  }
+  /**check if product is available
+   * @param {variant[]} variants
+   * @return {*}  {boolean}
+   * @memberof ListComponent
+   */
+  isAvailable(variants: variant[]): boolean {
+    var res = false;
+    for (var variant in variants) {
+      if (variants[variant].quantity > 0) res = true;
+    }
+    return res;
   }
 }
